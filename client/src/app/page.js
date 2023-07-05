e5ed7411366618a5b9eccab16b8e2e60f33e50db
Kalpayatri <115443803+Kalpayatri@users.noreply.github.com>
2023-07-05 14:48:43 +0530

    initial setup (#1)
    Co-authored-by: Khushboo Mallick <khushboomallick@Khushboos-MacBook-Air.local>

---
+++ client/src/app/page.js
@@ -0,0 +1,6 @@
+import './globals.css'
+
+
+export default function Page() {
+  return <h1 className="h-screen flex justify-center items-center  text-indigo-600 text-lg font-semibold">Hello, Next.js!</h1>
+}
